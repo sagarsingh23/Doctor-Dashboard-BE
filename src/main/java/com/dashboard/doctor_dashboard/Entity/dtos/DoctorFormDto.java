b20f87008e6607e87d19b009378bf65073440685
pranay-nareddy <pranay.nareddy@nineleaps.com>
2022-03-28 11:19:21 +0530

    added validation and new dto's

---
+++ src/main/java/com/dashboard/doctor_dashboard/Entity/dtos/DoctorFormDto.java
@@ -0,0 +1,26 @@
+package com.dashboard.doctor_dashboard.Entity.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorFormDto {
+
+    @NotNull
+    private long id;
+//    @Range(min = 24,max = 100,message = "enter age between 24-100")
+    private Short age;
+//    @Pattern(regexp = "^((?i)ortho|neuro|dermo|general|gastro)" ,message ="select from specified category [ortho,neuro,general,gastro,dermo]")
+    private String speciality;
+//    @Pattern(regexp= "^((?i)male|female|others)", message = "select from specified gender [male,female,others]")
+    private String gender;
+//    @Pattern(regexp = "^([0-9]{10})",message = "number should be of 10 digits")
+    private String phoneNo;
+}
---
+++ src/main/java/com/dashboard/doctor_dashboard/Entity/dtos/DoctorListDto.java
@@ -0,0 +1,14 @@
+package com.dashboard.doctor_dashboard.Entity.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorListDto {
+    private long id;
+    private String name;
+
+}
