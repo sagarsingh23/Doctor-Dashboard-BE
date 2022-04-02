e66d3d08a136be27f3ea633112666b3fda0acaee
sagarsingh24 <sagar.singh@nineleaps.com>
2022-04-02 17:17:37 +0530

    added new Exception classes..

--- src/main/java/com/dashboard/doctor_dashboard/exception/ErrorDetails.java
+++ src/main/java/com/dashboard/doctor_dashboard/exception/ErrorDetails.java
@@ -2,7 +2,7 @@ package com.dashboard.doctor_dashboard.exception;
 
 import java.util.Date;
 
-public class ErrorDetails extends  RuntimeException{
+public class ErrorDetails{
     private Date timestamp;
     private String message;
     private String details;
--- src/main/java/com/dashboard/doctor_dashboard/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dashboard/doctor_dashboard/exception/GlobalExceptionHandler.java
@@ -3,33 +3,19 @@ package com.dashboard.doctor_dashboard.exception;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler  {
-
-    //    @Override
-    //    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-    //        List list = ex.getBindingResult().getAllErrors().stream()
-    //                .map(fieldError -> fieldError.getDefaultMessage())
-    //                .collect(Collectors.toList());
-    //        ApiError apiError =
-    //                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), list);
-    //        return handleExceptionInternal(
-    //                ex, apiError, headers, apiError.getStatus(), request);
-    //    }
 
     //     handle specific exceptions
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -40,13 +26,7 @@ public class GlobalExceptionHandler  {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(ErrorDetails.class)
-    public ResponseEntity<ErrorDetails> notLoggedIn(ErrorDetails exception,WebRequest webRequest){
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
-                webRequest.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
 
-    }
     @ExceptionHandler(APIException.class)
     public ResponseEntity<ErrorDetails> handleAPIException(APIException exception,
                                                            WebRequest webRequest) {
@@ -84,6 +64,17 @@ public class GlobalExceptionHandler  {
     @ExceptionHandler(ReportNotFound.class)
     @ResponseBody
     public ResponseEntity<String> handleReportNotFoundException(ReportNotFound e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDetails> validation(final ConstraintViolationException ex,WebRequest webRequest){
+
+        List newList = ex.getConstraintViolations().stream().map(list -> list.getMessageTemplate()).collect(Collectors.toList());
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),newList.toString(),webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails,HttpStatus.BAD_REQUEST);
+
     }
 }
