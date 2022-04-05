a5ac130ff0c972b45853ee65b140d66f29cc0a79
sagarsingh24 <sagar.singh@nineleaps.com>
2022-04-05 16:08:29 +0530

    code fixes..

--- src/main/java/com/dashboard/doctor_dashboard/exception/MyCustomException.java
+++ src/main/java/com/dashboard/doctor_dashboard/exception/MyCustomException.java
@@ -6,7 +6,7 @@ public class MyCustomException extends RuntimeException {
     private long fieldValue;
 
     public MyCustomException(String resourceName, String fieldName, long fieldValue) {
-        super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue)); // Post not found with id : 1
+        super(String.format("%s not found in Doctor List with %s : %s", resourceName, fieldName, fieldValue)); // Post not found with id : 1
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
