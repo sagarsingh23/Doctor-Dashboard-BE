94c64d0840136eb4b11ba612f46004780bbe85af
sagarsingh24 <sagar.singh@nineleaps.com>
2022-04-21 16:23:38 +0530

    code fix..

--- src/main/java/com/dashboard/doctor_dashboard/Service/patient_service/Impl/AttributeServiceImpl.java
+++ src/main/java/com/dashboard/doctor_dashboard/Service/patient_service/Impl/AttributeServiceImpl.java
@@ -19,7 +19,7 @@ public class AttributeServiceImpl implements AttributeService {
 
     @Override
     public String changeNotes(Long id, String notes) {
-        if(patientRepository.getId(id) == id) {
+        if(patientRepository.getId(id).equals(id)) {
             attributeRepository.changeNotes(id, notes);
             return "Notes updated!!!";
         }
