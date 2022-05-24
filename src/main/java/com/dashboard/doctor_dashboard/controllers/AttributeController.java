63f48ab5442d08b70ddf0ba914d1415d3cad6901
sagarsingh24 <sagar.singh@nineleaps.com>
2022-05-24 20:01:32 +0530

    code fixes..

--- src/main/java/com/dashboard/doctor_dashboard/controllers/AttributeController.java
+++ src/main/java/com/dashboard/doctor_dashboard/controllers/AttributeController.java
@@ -1,7 +1,8 @@
 package com.dashboard.doctor_dashboard.controllers;
 
+import com.dashboard.doctor_dashboard.entities.Attributes;
 import com.dashboard.doctor_dashboard.entities.dtos.GenericMessage;
-import com.dashboard.doctor_dashboard.entities.dtos.NotesDto;
+import com.dashboard.doctor_dashboard.entities.dtos.PrescriptionDto;
 import com.dashboard.doctor_dashboard.services.patient_service.AttributeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,13 @@ public class AttributeController {
 
 
     @PutMapping("/changeNotes/{id}")
-    public ResponseEntity<GenericMessage> updateNotes(@PathVariable("id") Long id, @RequestBody NotesDto notes) { //updateNotes
-        return attributeService.changeNotes(id, notes.getNotes());
+    public ResponseEntity<GenericMessage> updatePrescription(@PathVariable("id") Long appointId, @RequestBody PrescriptionDto notes) { //updateNotes
+        return attributeService.changeNotes(appointId, notes.getPrescription());
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<GenericMessage> updateAttributes(@PathVariable("id") Long appointId, @RequestBody Attributes attributes){
+        return attributeService.updateAttributeByAppointmentId(appointId,attributes);
     }
 
 }
