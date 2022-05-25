153ce6cd970ec9daf269ea55f3366384372ec0ad
pranay-nareddy <pranay.nareddy@nineleaps.com>
2022-05-25 11:33:37 +0530

    added exception for 403 error

---
+++ src/main/java/com/dashboard/doctor_dashboard/jwt/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,69 @@
+package com.dashboard.doctor_dashboard.jwt.security;
+
+
+import com.dashboard.doctor_dashboard.entities.dtos.GenericMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomAuthenticationEntryPoint implements AccessDeniedHandler {
+
+//    @Override
+//    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        var jsonObject = new JSONObject();
+//        try {
+//            jsonObject.put("timestamp", System.currentTimeMillis());
+//            jsonObject.put("status", 403);
+//            jsonObject.put("message", "Access denied");
+//        } catch (JSONException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        res.setContentType("application/json;charset=UTF-8");
+//        res.setStatus(403);
+//
+//        res.getWriter().write(mapper.writeValueAsString(jsonObject));
+//    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ObjectMapper mapper = new ObjectMapper();
+//        var jsonObject = new JSONObject();
+        GenericMessage genericMessage=new GenericMessage();
+
+//        try {
+//            jsonObject.put("timestamp", System.currentTimeMillis());
+//            jsonObject.put("status", 403);
+//            jsonObject.put("message", "Access denied");
+//        } catch (JSONException e) {
+//            throw new RuntimeException(e);
+//        }
+        Map<String,Object> error =new HashMap<>();
+        error.put("timestamp", new Date().toString());
+        error.put("status",403);
+        error.put("message","Access denied");
+        genericMessage.setStatus("Forbidden");
+        genericMessage.setData(error);
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(403);
+
+        response.getWriter().write(mapper.writeValueAsString(genericMessage));
+
+    }
+}
