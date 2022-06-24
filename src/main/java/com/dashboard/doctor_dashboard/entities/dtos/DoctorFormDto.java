52284ac2b6a9411488c0252db688b04533ad28f5
pranay-nareddy <pranay.nareddy@nineleaps.com>
2022-06-24 15:29:09 +0530

    updated doctor speciality

--- src/main/java/com/dashboard/doctor_dashboard/entities/dtos/DoctorFormDto.java
+++ src/main/java/com/dashboard/doctor_dashboard/entities/dtos/DoctorFormDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -16,13 +17,16 @@ public class DoctorFormDto {
     private Long id;
     @Range(min = 24, max = 100, message = "Enter age between 24-100")
     private Short age;
-    @Pattern(regexp = "^((?i)Orthology|Dentist|General|gastrology|Dermatologist)", message = "Select from specified speciality [orthology,Dentist,Dermatologist,general,gastrology]")
+    @Pattern(regexp = "^((?i)Orthologist|Dentist|General|Gastrologist|Dermatologist)", message = "Select from specified speciality [Orthologist,Dentist,Dermatologist,General,Gastrologist]")
     private String speciality;
-    @Pattern(regexp = "^((?i)male|female|others)", message = "Select from specified gender [male,female,others]")
+    @Pattern(regexp = "^((?i)male|female|others)", message = "Select from specified gender [Male,Female,Others]")
     private String gender;
     @Pattern(regexp = "^([0-9]{10})", message = "Number should be of 10 digits")
     private String phoneNo;
+    @NotNull
     private Short exp;
+    @NotNull
+    @NotEmpty
     private String degree;
 
 }
