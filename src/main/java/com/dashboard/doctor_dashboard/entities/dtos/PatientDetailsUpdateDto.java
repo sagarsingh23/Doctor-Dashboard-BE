864b46a2acc880b0d996a73474c6b57b25aa75ae
pranay-nareddy <pranay.nareddy@nineleaps.com>
2022-06-24 15:27:34 +0530

    added patientDetailsUpdateDto

---
+++ src/main/java/com/dashboard/doctor_dashboard/entities/dtos/PatientDetailsUpdateDto.java
@@ -0,0 +1,15 @@
+package com.dashboard.doctor_dashboard.entities.dtos;
+
+import com.dashboard.doctor_dashboard.entities.Attributes;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class PatientDetailsUpdateDto {
+
+  private String mobileNo;
+
+}
