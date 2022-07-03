c10fcbd40941db1141c6c37b3cca68d0885d1e00
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-03 20:47:16 +0530

    unit test for services

--- src/test/java/com/dashboard/doctor_dashboard/services/JwtServiceImplTest.java
+++ src/test/java/com/dashboard/doctor_dashboard/services/JwtServiceImplTest.java
@@ -14,7 +14,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -48,28 +52,39 @@ class JwtServiceImplTest {
         String token = "ejsdetyfrbendsntsqodqpepmmgrewfetesdy";
 
         Login login = new Login();
-        login.setEmail("sagarssn23@gmail.com");
+        login.setEmail("sagar.singh@nineleaps.com");
         login.setId(1L);
         login.setUsername("sagar24");
+        login.setRole("DOCTOR");
+        login.setProfilePic("profilePic");
 
         Claims claims = new Claims();
         claims.setDoctorId(login.getId());
         claims.setDoctorEmail(login.getEmail());
         claims.setDoctorName(login.getUsername());
+        claims.setRole(login.getRole());
+        claims.setProfilePic(login.getProfilePic());
+
+
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(login.getRole()));
+        System.out.println(authorities);
 
         Authentication authentication = mock(Authentication.class);
 
         Mockito.when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                login.getEmail(),login.getUsername()
+                login.getEmail(),login.getUsername(),authorities
         ))).thenReturn(authentication);
+        System.out.println(authentication.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
 
-
-        Mockito.when(jwtTokenProvider.generateToken(authentication, claims)).thenReturn(token);
+        Mockito.when(jwtTokenProvider.generateToken(Mockito.any(Authentication.class),Mockito.any(Claims.class))).thenReturn(token);
 
         String newToken = jwtService.authenticateUser(login);
+        System.out.println(newToken);
 
         assertEquals(token,newToken);
         assertEquals(authentication,SecurityContextHolder.getContext().getAuthentication());
