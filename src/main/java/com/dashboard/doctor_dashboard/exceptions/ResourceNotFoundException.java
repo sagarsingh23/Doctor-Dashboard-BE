19027169bc4df8216b7403192a2cec4b21ebf029
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-03 12:22:37 +0530

    Modified Exception..

--- src/main/java/com/dashboard/doctor_dashboard/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/dashboard/doctor_dashboard/exceptions/ResourceNotFoundException.java
@@ -3,17 +3,9 @@ package com.dashboard.doctor_dashboard.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    public final String resourceName;
-    public final String fieldName;
-    public final long fieldValue;
-
-    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
-        super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue)); // Post not found with id : 1
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
-
 }
