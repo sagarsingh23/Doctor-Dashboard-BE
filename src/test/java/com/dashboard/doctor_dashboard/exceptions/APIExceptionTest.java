6ce93f0afa8d240bf60196f5bbb19b84af1975e6
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-06 23:34:31 +0530

    deleted exception test..

--- src/test/java/com/dashboard/doctor_dashboard/exceptions/APIExceptionTest.java
+++ src/test/java/com/dashboard/doctor_dashboard/exceptions/APIExceptionTest.java
@@ -1,17 +1,17 @@
-package com.dashboard.doctor_dashboard.exceptions;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
-public class APIExceptionTest {
-
-    @Test(expected = APIException.class)
-    public void apiExceptionTest() {
-        throw new APIException("test for API exception, error code: 405");
-    }
-
-}
+//package com.dashboard.doctor_dashboard.exceptions;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//@RunWith(MockitoJUnitRunner.class)
+//public class APIExceptionTest {
+//
+//    @Test(expected = APIException.class)
+//    public void apiExceptionTest() {
+//        throw new APIException("test for API exception, error code: 405");
+//    }
+//
+//}
--- src/test/java/com/dashboard/doctor_dashboard/exceptions/ResourceNotFoundExceptionTest.java
+++ src/test/java/com/dashboard/doctor_dashboard/exceptions/ResourceNotFoundExceptionTest.java
@@ -1,17 +1,17 @@
-package com.dashboard.doctor_dashboard.exceptions;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
-public class ResourceNotFoundExceptionTest {
-
-    @Test(expected = ResourceNotFoundException.class)
-    public void resourceNotFoundTest() {
-        throw new ResourceNotFoundException("test for resource not found, error code: 404");
-    }
-
-}
+//package com.dashboard.doctor_dashboard.exceptions;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//@RunWith(MockitoJUnitRunner.class)
+//public class ResourceNotFoundExceptionTest {
+//
+//    @Test(expected = ResourceNotFoundException.class)
+//    public void resourceNotFoundTest() {
+//        throw new ResourceNotFoundException("test for resource not found, error code: 404");
+//    }
+//
+//}
