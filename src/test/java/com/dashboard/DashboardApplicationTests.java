641174538d39804273c0c193af97c97d1fa4de33
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-06 23:51:05 +0530

    deleted file

--- src/test/java/com/dashboard/DashboardApplicationTests.java
+++ src/test/java/com/dashboard/DashboardApplicationTests.java
@@ -1,13 +1,17 @@
-package com.dashboard;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DashboardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.dashboard;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@SpringBootTest
+//class DashboardApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//		String message = "new class";
+//		assertEquals(message,"new class");
+//	}
+//
+//}
