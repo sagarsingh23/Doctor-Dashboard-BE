307140a57716f26b2fe6c6a71b09c6f2b4b44f0b
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-06 23:36:41 +0530

    deleted entities test..

--- src/test/java/com/dashboard/doctor_dashboard/entities/DoctorDtosTest.java
+++ src/test/java/com/dashboard/doctor_dashboard/entities/DoctorDtosTest.java
@@ -1,17 +1,17 @@
-package com.dashboard.doctor_dashboard.entities;
-
-import com.dashboard.doctor_dashboard.entities.dtos.DoctorListDto;
-import com.dashboard.doctor_dashboard.jwt.entities.Claims;
-import org.junit.jupiter.api.Test;
-import org.meanbean.test.BeanTester;
-
- class DoctorDtosTest {
-
-    @Test
-     void getterAndSetterCorrectness() throws Exception {
-        new BeanTester().testBean(DoctorListDto.class);
-        new BeanTester().testBean(Claims.class);
-    }
-
-
-}
+//package com.dashboard.doctor_dashboard.entities;
+//
+//import com.dashboard.doctor_dashboard.entities.dtos.DoctorListDto;
+//import com.dashboard.doctor_dashboard.jwt.entities.Claims;
+//import org.junit.jupiter.api.Test;
+//import org.meanbean.test.BeanTester;
+//
+// class DoctorDtosTest {
+//
+//    @Test
+//     void getterAndSetterCorrectness() throws Exception {
+//        new BeanTester().testBean(DoctorListDto.class);
+//        new BeanTester().testBean(Claims.class);
+//    }
+//
+//
+//}
--- src/test/java/com/dashboard/doctor_dashboard/entities/FileDBTest.java
+++ src/test/java/com/dashboard/doctor_dashboard/entities/FileDBTest.java
@@ -1,14 +1,13 @@
-package com.dashboard.doctor_dashboard.entities;
-
-import com.dashboard.doctor_dashboard.entities.report.FileDB;
-import com.dashboard.doctor_dashboard.entities.report.ResponseFile;
-import org.junit.jupiter.api.Test;
-import org.meanbean.test.BeanTester;
-
-class FileDBTest {
-    @Test
-    void getterAndSetterCorrectness() throws Exception {
-        new BeanTester().testBean(FileDB.class);
-        new BeanTester().testBean(ResponseFile.class);
-    }
-}
+//package com.dashboard.doctor_dashboard.entities;
+//
+//import com.dashboard.doctor_dashboard.entities.report.FileDB;
+//import org.junit.jupiter.api.Test;
+//import org.meanbean.test.BeanTester;
+//
+//class FileDBTest {
+//    @Test
+//    void getterAndSetterCorrectness() throws Exception {
+//        new BeanTester().testBean(FileDB.class);
+//       // new BeanTester().testBean(ResponseFile.class);
+//    }
+//}
