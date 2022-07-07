7efd87ec3ebac44ecd353b55a302e9c22b6c351f
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-07 16:39:02 +0530

    unit tests for todolist service

--- src/test/java/com/dashboard/doctor_dashboard/services/TodoServiceImplTest.java
+++ src/test/java/com/dashboard/doctor_dashboard/services/TodoServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.dashboard.doctor_dashboard.services;
 
+import com.dashboard.doctor_dashboard.entities.dtos.TodoListDto;
 import com.dashboard.doctor_dashboard.entities.model.Todolist;
 import com.dashboard.doctor_dashboard.entities.wrapper.GenericMessage;
 import com.dashboard.doctor_dashboard.repository.TodoRepository;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
@@ -28,8 +30,13 @@ class TodoServiceImplTest {
     @Mock
     private TodoRepository todoRepository;
 
+    @Mock
+    private ModelMapper mapper;
+
     @InjectMocks
     private TodoServiceImpl todoService;
+
+
 
 
     @BeforeEach
@@ -47,9 +54,12 @@ class TodoServiceImplTest {
 
     @Test
     void testAddList() {
-        Todolist todolist = new Todolist(1L,"hello",true,null,null,null);
+        TodoListDto todolist = new TodoListDto(1L,"hello",true,null);
+        Todolist todolist1 = new Todolist(1L,"hello",true,null,null,null);
 
-        Mockito.doReturn(todolist).when(todoRepository).save(Mockito.any(Todolist.class));
+
+        Mockito.when(mapper.map(todolist,Todolist.class)).thenReturn(todolist1);
+        Mockito.doReturn(todolist1).when(todoRepository).save(Mockito.any(Todolist.class));
 
         ResponseEntity<GenericMessage> newTodo = todoService.addTodo(todolist);
 
@@ -104,11 +114,14 @@ class TodoServiceImplTest {
     }
 
     @Test
-    void updatelist() {
+    void updateList() {
         final Long id = 1L;
-        Todolist todolist = new Todolist(1L,"hello",true,null,null,null);
 
-        Mockito.when(todoRepository.findById(id)).thenReturn(Optional.of(todolist));
+        TodoListDto todolist = new TodoListDto(1L,"hello",true,null);
+        Todolist todolist1 = new Todolist(1L,"hello",true,null,null,null);
+
+        Mockito.when(todoRepository.findById(id)).thenReturn(Optional.of(todolist1));
+
         todoService.updateTodo(id,todolist);
         todoService.updateTodo(id,todolist);
 
@@ -120,7 +133,7 @@ class TodoServiceImplTest {
     @Test
     void checkIfIdNotPresentInDBForUpdateList() {
         final Long id = 1L;
-        Todolist todolist = new Todolist(id,"hello",true,null,null,null);
+        TodoListDto todolist = new TodoListDto(1L,"hello",true,null);
 
         Mockito.when(todoRepository.findById(id)).thenReturn(Optional.empty());
 
