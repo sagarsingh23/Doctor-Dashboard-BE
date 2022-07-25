316820ae972c5dc47285e8908e79c91248c1d082
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-25 23:03:09 +0530

    Updated Constants.java

--- src/main/java/com/dashboard/doctor_dashboard/utils/Constants.java
+++ src/main/java/com/dashboard/doctor_dashboard/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String SUCCESS = "success";
     public static final String FAIL = "failure";
     public static final String LOGIN = "Login";
-
+    public static final String  DEFAULT_PAGE_SIZE="10";
 
     public static final String DETAILS_MISMATCH = "details provided is not correct";
 
@@ -44,49 +44,6 @@ public class Constants {
 
     public static final String MAIL_ERROR = "mail error!!! cant connect with the host server";
     public static final String PRESCRIPTION_CREATED = "prescription added";
-
-
-    public static final String MAIL_PRESCRIPTION = """
-            <html>
-            <head></head>
-            <body>
-            <div ><div style="background-color:white;color:black"><div>
-             </div><p style="text-align:left;font-size:15px">Hi Sagar,</p>
-             <p>
-            Your appointment has been booked. Check the details given below.</p><table><tbody><tr><th>Doctor Name</th><th>Doctor Email</th><th>Speciality</th><th>Date of Appointment</th><th>Appointment Time</th></tr><tr><td>sagar</td><td><a href="mailto:sagar.singh@nineleaps.com" target="_blank">sagar.singh@nineleaps.com</a></td><td>General</td><td>2022-06-28</td><td>12:30</td></tr></tbody></table> <p style="text-align:left;font-size:13px">
-             For further queries, please mail to:
-             <span style="color:#fffff"><a href="mailto:mecareapplication@gmail.com" target="_blank">mecareapplication@gmail.com</a></span>
-             </p>
-             <p style="text-align:left;font-size:13px;line-height:0.8">
-             Thanks &amp; Regards, </p>
-             <p style="font-size:13px;text-align:left;line-height:0.8"><span class="il">meCare</span> Application team</p></div><div class="yj6qo"></div><div class="adL">
-            </div></div>
-             
-            </body>
-            </html>
-            """;
-
-    public static final String MAIL_APPOINTMENT = "<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;padding: 15px;margin-top: auto; }"
-            + "</style></head>"
-            + "<div style=\"background-color: white; color:black  \">\n"
-            + " <p style=\"text-align: left; font-size:15px ;\">Hi [[name]],</p>\n"
-            + " <p style =\"text-align:left; font-size:15px ;line-height: 0.8\n"
-            + " font-family: 'Arial' \n" + " ;\n"
-            + " \"\n" + " >\n"
-            + "Your appointment has been booked. Check the details given below.</p>"
-
-            + "<table><tr><th>Doctor Name</th><th>Doctor Email</th><th>Speciality</th><th>Date of Appointment</th><th>Appointment Time</th></tr><tr><td>Dr.[[doctorName]]</td><td>[[doctorEmail]]</td><td>[[speciality]]</td><td>[[dateOfAppointment]]</td><td>[[appointmentTime]]</td></tr></table>"
-
-            + " <p style=\" text-align: left ;font-size:13px \">\n"
-            + " For further queries, please mail to:\n" + " <span style=\"color: #FFFFF; \"\n"
-            + " >mecareapplication@gmail.com</span\n" + " >\n" + " </p>\n"
-            + " <p style=\" text-align: left;font-size:13px;line-height: 0.8\">\n"
-            + " Thanks & Regards, </p>\n"
-            + " <p style=\"font-size: 13px; text-align: left;line-height: 0.8\">meCare team</span\n"
-            + " </div>";
-
-
-
 
     private Constants() {
         throw new IllegalArgumentException();
