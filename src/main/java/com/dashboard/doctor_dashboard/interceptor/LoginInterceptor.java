80c8ca4504adcea999ac4e6e711d1fca910f9936
sagarsingh24 <sagar.singh@nineleaps.com>
2022-07-31 15:52:29 +0530

    added interceptor

---
+++ src/main/java/com/dashboard/doctor_dashboard/interceptor/LoginInterceptor.java
@@ -0,0 +1,60 @@
+package com.dashboard.doctor_dashboard.interceptor;
+
+import com.dashboard.doctor_dashboard.exceptions.APIException;
+import com.dashboard.doctor_dashboard.jwt.security.JwtTokenProvider;
+import com.dashboard.doctor_dashboard.repository.LoginRepo;
+import com.dashboard.doctor_dashboard.utils.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+
+@Component
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+    private JwtTokenProvider tokenProvider;
+
+    private LoginRepo loginRepo;
+
+    @Autowired
+    public LoginInterceptor(JwtTokenProvider tokenProvider, LoginRepo loginRepo) {
+        this.tokenProvider = tokenProvider;
+        this.loginRepo = loginRepo;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        try {
+            int userId = Integer.parseInt(request.getHeader("userId"));
+            String jwtToken;
+            String bearerToken = request.getHeader("Authorization");
+            if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+                jwtToken = bearerToken.substring(7);
+                tokenProvider.validateToken(jwtToken, request);
+                String username = tokenProvider.getUsernameFromJWT(jwtToken);
+                int id = loginRepo.getId(username);
+                if (userId == id) {
+                    log.debug("token successfully authenticated");
+                    return true;
+                }
+                throw new APIException(Constants.UNAUTHORIZED);
+            }else{
+                throw new APIException("JWT claims string is empty.");
+            }
+        } catch (NumberFormatException e){
+
+            throw new APIException("userId is missing in header!!!");
+      }
+   }
+
+
+}
